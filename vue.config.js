cfb6309ac33f0d7cca96721b0826e27cdaa4a767
ShallowDream223 <2246340200@qq.com>
2023-02-13 18:11:30 +0800

    65656

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-
+  publicPath: "./",
   devServer: {
     port: 8080, // 修改默认端口
     proxy: {
       // 代理转发
       "/api": {
-
         target: "http://localhost",
         // target: "http://192.168.28.230",
         changeOrigin: true, // 本地是否开启代理
